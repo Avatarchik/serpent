d1758de3c6efad76ba61b83f1aa644d96917256c
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2015-07-30 00:48:57 +0300

    Added Utils static class for small auxiliary functions

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Utils {
+    public static Color VectorToColor(Vector3 vec) {
+        return new Color(vec.x, vec.y, vec.z);
+    }
+}
