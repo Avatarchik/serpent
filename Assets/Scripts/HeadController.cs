92bdb5ffc2f0681e1dda96f8fa03359fe53a2cc7
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2015-08-28 17:41:55 +0300

    Added HeadController with CastToSurface() and stub for 3rd person camera

---
+++ Assets/Scripts/HeadController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Snake3D {
+
+    public class HeadController : MonoBehaviour {
+
+        public UI.Joystick joystick;
+        public Geometry.PlanetGeometry planetGeometry;
+
+        void Start() {
+            CastToSurface();
+        }
+
+        void Update() {
+
+        }
+
+        void CastToSurface() {
+            Vector3 radiusVec = transform.position.normalized;
+            float height = planetGeometry.GetHeightAt(radiusVec);
+            transform.position = radiusVec * height;
+
+            // TODO: Respect surface normal
+        }
+    }
+
+} // namespace Snake3D
---
+++ Assets/Scripts/TPSCameraRig.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Snake3D {
+
+    /*
+     * Follows HeadController but without X rotation.
+     */
+    public class TPSCameraRig : MonoBehaviour {
+
+        public Transform headController;
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+    }
+
+} // namespace Snake3D
