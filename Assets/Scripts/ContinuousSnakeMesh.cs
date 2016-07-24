ec84b25415454ac44c2523d322324ad68d534225
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2016-07-24 07:18:21 +0300

    Implement head patch
    This should be refactored later to use transform matrices from SnakeKernel
    instead of remembering it in SnakeMesh.

--- Assets/Scripts/ContinuousSnakeMesh.cs
+++ Assets/Scripts/ContinuousSnakeMesh.cs
@@ -14,13 +14,22 @@ public class ContinuousSnakeMesh : MonoBehaviour, IInitializable, IGrowable {
     public float interval = 0.25f;
 
     public ISnakeMesh snakeMesh;
+
     private bool initialized = false;
     private Vector3 lastGrowPoint;
-    
+
+    private ISnakeMesh headPatch;
+    private Matrix4x4 lastTransformMat; // TODO: remove when SnakeKernel will contain all transform matrices
+
     public void Init() {
         Debug.Assert(snakeMesh_ != null);
         snakeMesh = snakeMesh_ as ISnakeMesh;
         Debug.Assert(snakeMesh != null);
+
+        Transform headPatchTransform = transform.FindChild("Head Patch");
+        Debug.Assert(headPatchTransform != null);
+        headPatch = headPatchTransform.GetComponent<SnakeMesh>();
+        Debug.Assert(headPatch != null);
     }
 
 
@@ -30,14 +39,15 @@ public class ContinuousSnakeMesh : MonoBehaviour, IInitializable, IGrowable {
         if (!initialized) {
             initialized = true;
             lastGrowPoint = dest;
+
+            // TODO: remove
+            lastTransformMat = localToWorld;
+            headPatch.PushToEnd(localToWorld, distanceTraveled);
+            headPatch.PushToEnd(localToWorld, distanceTraveled);
         }
 
-        // Grow if needed
-        Vector3 delta = dest - lastGrowPoint;
-        if (delta.magnitude >= interval) {
-            lastGrowPoint += delta.normalized * interval;
-            snakeMesh.PushToEnd(localToWorld, distanceTraveled);
-        }
+        GrowBodyMesh(localToWorld, distanceTraveled);
+        UpdateHeadPatch(localToWorld, distanceTraveled);
 
         AnimateUV(distanceTraveled);
     }
@@ -60,9 +70,37 @@ public class ContinuousSnakeMesh : MonoBehaviour, IInitializable, IGrowable {
     }
 
 
+    // Grows body mesh if needed in current frame.
+    private void GrowBodyMesh(Matrix4x4 localToWorld, float distanceTraveled) {
+        Vector3 dest = localToWorld.MultiplyPoint3x4(Vector3.zero);
+
+        Vector3 delta = dest - lastGrowPoint;
+        if (delta.magnitude >= interval) {
+            // Grow
+            lastGrowPoint += delta.normalized * interval;
+            snakeMesh.PushToEnd(localToWorld, distanceTraveled);
+
+            // TODO: remove in future
+            lastTransformMat = localToWorld;
+        }
+    }
+
+    private void UpdateHeadPatch(Matrix4x4 localToWorld, float distanceTraveled) {
+        headPatch.PopFromStart();
+        headPatch.PopFromStart();
+        
+        Vector3 currentPosition = localToWorld.MultiplyPoint3x4(Vector3.zero);
+        Vector3 lastPosition = lastTransformMat.MultiplyPoint3x4(Vector3.zero);
+        float lastDistance = distanceTraveled - (currentPosition - lastPosition).magnitude;
+
+        headPatch.PushToEnd(lastTransformMat, lastDistance);
+        headPatch.PushToEnd(localToWorld, distanceTraveled);
+    }
+
     private void AnimateUV(float distanceTraveled) {
         Vector2 offset = snakeMesh.TextureOffset;
         offset.y = -distanceTraveled / snakeMesh.RingLength;
         snakeMesh.TextureOffset = offset;
+        headPatch.TextureOffset = offset;
     }
 }
