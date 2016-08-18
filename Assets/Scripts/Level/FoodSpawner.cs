fa53f8fb308980138cc22fba5292b119e47e224b
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2016-08-18 08:11:51 +0300

    Move food spawn code out of SnakeHead

---
+++ Assets/Scripts/Level/FoodSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodSpawner : MonoBehaviour, IInitializable {
+
+    public GameObject foodPrefab;
+    public FoodPointer foodPointer;
+
+    public static FoodSpawner instance { get; private set; }
+
+    private Terrain terrain;
+
+    public void Init() {
+        Debug.Assert(instance == null);
+        instance = this;
+
+        Debug.Assert(foodPrefab != null);
+        Debug.Assert(foodPointer != null);
+
+        terrain = FindObjectOfType<Terrain>();
+        Debug.Assert(terrain != null);
+
+
+        SpawnNewFood();
+    }
+
+    public void SpawnNewFood() {
+        var position = new Vector3();
+
+        Vector3 size = terrain.terrainData.size;
+        float padding = 1;
+        position.x = Random.Range(padding, size.x - padding);
+        position.z = Random.Range(padding, size.z - padding);
+
+        // Get normalized terrain coordinates
+        float x = position.x / size.x;
+        float y = position.z / size.z;
+        position.y = terrain.terrainData.GetInterpolatedHeight(x, y);
+
+        var gameObject = Instantiate(foodPrefab, position, Quaternion.identity) as GameObject;
+        foodPointer.food = gameObject.transform;
+    }
+}
--- Assets/Scripts/SnakeHead.cs
+++ Assets/Scripts/SnakeHead.cs
@@ -3,56 +3,32 @@
 [RequireComponent(typeof(AudioSource))]
 public class SnakeHead : MonoBehaviour {
 
-    public GameObject foodPrefab;
     public GrowController growController;
     public float growDelta = 4;
-    public FoodPointer foodPointer;
 
-    private Terrain terrain;
     private AudioSource audioSource;
 
     void Start () {
-        Debug.Assert(foodPrefab != null);
         Debug.Assert(growController != null);
-        Debug.Assert(foodPointer != null);
 
-        terrain = FindObjectOfType<Terrain>();
-        Debug.Assert(terrain != null);
         audioSource = GetComponent<AudioSource>();
-
-        SpawnNewFood();
 	}
 
     void OnTriggerEnter(Collider other) {
-        if (!other.CompareTag("Food"))
+        if (!other.CompareTag("Food")) {
             // TODO: game over
             return;
-        
-        Destroy(other.transform.parent.gameObject);
+        }
+
+        OnFoodPickedUp(other.transform.parent.gameObject);
+    }
+
+    private void OnFoodPickedUp(GameObject food) {
+        Destroy(food);
         audioSource.Play();
 
-        SpawnNewFood();
-        GrowSnake();
-    }
-
-    private void GrowSnake() {
+        FoodSpawner.instance.SpawnNewFood();
+        // Grow snake
         growController.targetLength += growDelta;
-    }
-
-    private void SpawnNewFood() {
-        var position = new Vector3();
-
-        Vector3 size = terrain.terrainData.size;
-        float padding = 1;
-        position.x = Random.Range(padding, size.x - padding);
-        position.z = Random.Range(padding, size.z - padding);
-
-        // Get normalized terrain coordinates
-        float x = position.x / size.x;
-        float y = position.z / size.z;
-        position.y = terrain.terrainData.GetInterpolatedHeight(x, y);
-
-        var gameObject = Instantiate(foodPrefab, position, Quaternion.identity) as GameObject;
-        foodPointer.food = gameObject.transform;
     }
 }
